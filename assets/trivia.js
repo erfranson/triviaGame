44d25b3181fc0684f8bc11f7fca65b74cffe3f64
Ethan Franson <ethanfranson2@gmail.com>
2017-07-19 17:29:56 -0600

    timer

--- assets/trivia.js
+++ assets/trivia.js
@@ -3,22 +3,34 @@ var question2 = ["What is Nsync's most popular song on spotify?"];
 var question3 = ["What Video game console Has the most Sales?"];
 var question4 =["How did Marissa Cooper die on the OC?"];
 var q1 = ["George Washington ", "Donald Trump ", "Richard Nixon ", "Barrack Obama"];
-var q2 = ["It's Gonna Be Me ", "Bye Bye Bye ", "Pop ", "Tearin' up My Heart "]
-var q3 =["Game Boy ", "PS2 ", "Nintendo DS ", "Xbox 360 " ]
-var q4=["Shooting ", "Car Crash ", "Overdose ", "Downed "]
+var q2 = ["It's Gonna Be Me ", "Bye Bye Bye ", "Pop ", "Tearin' up My Heart "];
+var q3 =["Game Boy ", "PS2 ", "Nintendo DS ", "Xbox 360 " ];
+var q4=["Shooting ", "Car Crash ", "Overdose ", "Downed "];
+var count=45;
 var incorrect = 0;
 var correct = 0;
 var unanswered = 0;
 
-$('button').on('click', function(){
-	
+$('#trivia').click( function(){
+	$('button').hide();
+	var counter =setInterval(timer, 1000);
+	function timer() {
+	count = count-1;
+	if (count <= 0) {
+		clearInterval(counter);
+		$('.content').empty();
+		$('.content').append('<p> incorrect: '+ incorrect + '<p>' + '<p> correct: '+ correct +'<p>' + '<p> unanswered: '+ unanswered +'<p>');
+	}
+	$('.timer').html('<h3>'+ count + 'secs </h3>');
+}
+	timer();
 	quest1();
 	quest2();
 	quest3();
 	quest4();
 
 
-	$('button').hide();
+	
 });
 
 
@@ -26,43 +38,44 @@ function quest1(){
 	$.each(question1, function(i){
 		$('.content').append('<h2>' + question1[i] + '</h2>');
 		$('.content').append('<form action="" class= "radio1"></form>');
-	for (var i = 0; i < q1.length; i++) {
-    	$('.radio1').append(q1[i] +'<input type= "radio" name ="choice" value="' + q1[i] + '" class="choice"> '  );
+	for (var j = 0; j < q1.length; j++) {
+    	$('.radio1').append(q1[j] +'<input type= "radio" name ="choice" value="' + q1[j] + '" class="choice"> '  );
     	$('.choice').addClass('choice');
-    };
+    }
 	});
 	
-};
+}
 function quest2(){
 	$.each(question2, function(i){
 		$('.content').append('<h2>' + question2[i] + '</h2>');
 		$('.content').append('<form action="" class= "radio2"></form>');
-	for (var i = 0; i < q2.length; i++) {
-    	$('.radio2').append(q2[i] +'<input type= "radio" name ="choice" value="' + q2[i] + '" class="choice"> '  );
+	for (var j = 0; j < q2.length; j++) {
+    	$('.radio2').append(q2[j] +'<input type= "radio" name ="choice" value="' + q2[j] + '" class="choice"> '  );
     	$('.choice').addClass('choice');
-    };
+    }
 	});
 	
-};
+}
 function quest3(){
 	$.each(question3, function(i){
 		$('.content').append('<h2>' + question3[i] + '</h2>');
 		$('.content').append('<form action="" class= "radio3"></form>');
-	for (var i = 0; i < q3.length; i++) {
-    	$('.radio3').append(q3[i] +'<input type= "radio" name ="choice" value="' + q3[i] + '" class="choice"> '  );
+	for (var j = 0; j < q3.length; j++) {
+    	$('.radio3').append(q3[j] +'<input type= "radio" name ="choice" value="' + q3[j] + '" class="choice"> '  );
     	$('.choice').addClass('choice');
-    };
+    }
 	});
 	
-};
+}
 function quest4(){
 	$.each(question4, function(i){
 		$('.content').append('<h2>' + question4[i] + '</h2>');
 		$('.content').append('<form action="" class= "radio4"></form>');
-	for (var i = 0; i < q4.length; i++) {
-    	$('.radio4').append(q4[i] +'<input type= "radio" name ="choice" value="' + q4[i] + '" class="choice"> '  );
+	for (var j = 0; j < q4.length; j++) {
+    	$('.radio4').append(q4[j] +'<input type= "radio" name ="choice" value="' + q4[j] + '" class="choice"> '  );
     	$('.choice').addClass('choice');
-    };
+    }
 	});
 	
-};
+}
+
